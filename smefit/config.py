54ab109b05d16fa7c11434c9d3e6270b6df22bc3
Luca Mantani <luca.mantani@gmail.com>
2026-01-19 17:56:53 +0100

    added parse rule

--- smefit/config.py
+++ smefit/config.py
@@ -4,10 +4,23 @@ smefit.config.py
 Config module of smefit
 """
 
+import logging
+import pathlib
+
 from reportengine.configparser import Config
+
+log = logging.getLogger(__name__)
 
 
 class smefitConfig(Config):
     """smefit Config class."""
 
-    pass
+    def parse_data_path(self, data_path):
+        """Parse data path."""
+        data_path = pathlib.Path(data_path)
+        # Verify it exists
+        if not data_path.exists():
+            log.error(f"data_path {data_path} does not exist.")
+            raise ValueError(f"data_path {data_path} does not exist.")
+        log.info(f"Using data path: {data_path}")
+        return data_path

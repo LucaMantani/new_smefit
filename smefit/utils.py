5a40d7dabea586629248ebb7dd3374b3c123a56d
Luca Mantani <luca.mantani@gmail.com>
2026-01-20 21:53:04 +0100

    Added test of gradient

--- smefit/utils.py
+++ smefit/utils.py
@@ -8,6 +8,19 @@ import jax
 import jax.numpy as jnp
 
 
+def build_chi2(eft_model, data, fit_covmat):
+    inv_covmat = jnp.linalg.inv(fit_covmat)
+
+    def chi2_fn(coeffs):
+        """Compute chi2 given coefficient values."""
+        predictions = eft_model.forward_map(coeffs)
+        residuals = data.cv - predictions
+        chi2 = residuals.T @ inv_covmat @ residuals
+        return chi2
+
+    return chi2_fn
+
+
 def ensure_list(x):
     """Ensure the input is a list.
     If the input is not a list, wrap it in a list.
@@ -17,7 +30,7 @@ def ensure_list(x):
     return [x]
 
 
-def run_test(eft_model):
+def run_test(eft_model, build_chi2):
 
     print(eft_model.coefficients.free_names)
     free = jnp.array([5.0, -3.0])
@@ -27,3 +40,8 @@ def run_test(eft_model):
     print(eft_model.derive_coeffs(free))
 
     print(eft_model.forward_map(free))
+
+    print(build_chi2(free))
+    # compute gradient
+    grad_chi2 = jax.grad(build_chi2)(free)
+    print(grad_chi2)

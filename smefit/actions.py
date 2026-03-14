49eb6e7493a54668afb964207ea921b39db229b3
Luca Mantani <luca.mantani@gmail.com>
2026-03-14 17:06:08 +0100

    Update analytic fit functionality to save results as JSON

--- smefit/actions.py
+++ smefit/actions.py
@@ -4,13 +4,18 @@ smefit.actions.py
 Reportengine actions for smefit.
 """
 
+from smefit.utils import write_fit_result
 
-def run_analytic_fit(analytic_fit):
-    """Print the result of the analytic fit.
+
+def run_analytic_fit(analytic_fit, output_path):
+    """Print and save the result of the analytic fit.
 
     Parameters
     ----------
     analytic_fit : FitResult
         Result produced by the ``analytic_fit`` provider node.
+    output_path : pathlib.Path
+        Path to the output directory for writing results to disk.
     """
     analytic_fit.print_summary()
+    write_fit_result(analytic_fit, output_path)

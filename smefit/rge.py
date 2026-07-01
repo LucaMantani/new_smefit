d35ad1eae68331793322fce84d0bbdc58ba90b04
Elie Hammou <elie.hammouvictoroff@gmail.com>
2026-07-01 14:02:34 +0200

    Added resolve_path step to RGE loading

--- smefit/rge.py
+++ smefit/rge.py
@@ -12,6 +12,7 @@ import pandas as pd
 import wilson
 
 from smefit.constants import gs, mz
+from smefit.paths import resolve_path
 from smefit.wcxf import inverse_wcxf_translate, wcxf_translate
 
 # Numerical threshold for filtering small Wilson coefficient values
@@ -638,6 +639,7 @@ def load_rge_matrix(
     # load precomputed RGE matrix if it exists
     path_to_rge_mat = rge_dict.get("rg_matrix", False)
     if path_to_rge_mat:
+        path_to_rge_mat = resolve_path(path_to_rge_mat)
         rge_cache = load_precomputed_rge_matrix(path_to_rge_mat, rge_settings)
 
     # compute or fetch the RGE matrix for each scale
